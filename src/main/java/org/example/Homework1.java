e7190448a47e584c628837d67af0839964cdb0d1
Kristy2594 <k.primakova2017@yandex.com>
2024-06-03 12:48:14 +0300

    homework1

--- src/main/java/org/example/Homework1.java
+++ src/main/java/org/example/Homework1.java
@@ -5,52 +5,68 @@ public class Homework1 {
 
         String poem = " Шаганэ ты моя, Шаганэ!Потому, что я с севера, что ли,Я готов рассказать тебе поле,Про волнистую рожь при луне.";
 
-        int result = poem.length(); //Возвращает длину строки
+        //Возвращает длину строки
+        int result = poem.length();
         System.out.println(result);
 
-        boolean result2 = poem.isEmpty(); //  позволяет проверить строку на пустоту. Если строка пуста, он возвращает true
+        //Позволяет проверить строку на пустоту. Если строка пуста, он возвращает true
+        boolean result2 = poem.isEmpty();
         System.out.println(result2);
 
-        boolean result3 = poem.isBlank(); // Работает со строками, которые null, пустые или содержат только пробелы
+        // Работает со строками, которые null, пустые или содержат только пробелы
+        boolean result3 = poem.isBlank();
         System.out.println(result3);
 
-        String result4 = poem.substring(1,4); //Метод извлекает символы между двумя индексами (позициями) из строки и возвращает подстроку.
+        //Метод извлекает символы между двумя индексами (позициями) из строки и возвращает подстроку
+        String result4 = poem.substring(1, 4);
         System.out.println(result4);
 
-        int result5 = poem.indexOf("у"); //Метод возвращает позицию первого появления значения в строке.
+        //Метод возвращает позицию первого появления значения в строке
+        int result5 = poem.indexOf("у");
         System.out.println(result5);
 
-        int result6 = poem.lastIndexOf("с"); //Метод возвращает индекс (позицию) последнего появления заданного значения в строке.
+        //Метод возвращает индекс (позицию) последнего появления заданного значения в строке
+        int result6 = poem.lastIndexOf("с");
         System.out.println(result6);
 
-        String result7 = poem.toLowerCase(); //Метод преобразует строку в строчные буквы.
+        //Метод преобразует строку в строчные буквы
+        String result7 = poem.toLowerCase();
         System.out.println(result7);
 
-        String result8 = poem.toUpperCase(); //Метод преобразует строку в прописные буквы.
+        //Метод преобразует строку в прописные буквы
+        String result8 = poem.toUpperCase();
         System.out.println(result8);
 
-        String result9 = poem.replace("Шаганэ", "Ирина"); //Метод возвращает новую строку с заменяемым значением
+        //Метод возвращает новую строку с заменяемым значением
+        String result9 = poem.replace("Шаганэ", "Ирина");
         System.out.println(result9);
 
-        boolean result10 = poem.startsWith("И");//Метод проверяет, начинается ли строка с указанного символа(ов)
+        //Метод проверяет, начинается ли строка с указанного символа(ов)
+        boolean result10 = poem.startsWith("И");
         System.out.println(result10);
 
-        boolean result11= poem.endsWith("."); //Метод проверяет, заканчивается ли строка указанным символом(ами).
+        //Метод проверяет, заканчивается ли строка указанным символом(ами).
+        boolean result11 = poem.endsWith(".");
         System.out.println(result11);
 
-        String result12 = poem.repeat(3); //Метод повтояет значение переменной заданное количество раз
+        //Метод повтояет значение переменной заданное количество раз
+        String result12 = poem.repeat(3);
         System.out.println(result12);
 
-        boolean result13 = poem.contains("урок"); //Метод проверяет, содержит ли строка последовательность символов.
+        //Метод проверяет, содержит ли строка последовательность символов
+        boolean result13 = poem.contains("урок");
         System.out.println(result13);
 
-        String result14 = poem.concat("Есенин"); //Метод добавляет строку к концу другой строки.
+        //Метод добавляет строку к концу другой строки
+        String result14 = poem.concat("Есенин");
         System.out.println(result14);
 
-        String result15 = poem.trim(); //Метод удаляет пробел с обоих концов строки.
+        //Метод удаляет пробел с обоих концов строки
+        String result15 = poem.trim();
         System.out.println(result15);
 
-        boolean result16 = poem.equals(" Шаганэ ты моя, Шаганэ!Потому, что я с севера, что ли,Я готов рассказать тебе поле,Про волнистую рожь при луне."); //Метод сравнивает две строки и возвращает true, если строки равны, и false, если нет.
+        //Метод сравнивает две строки и возвращает true, если строки равны, и false, если нет
+        boolean result16 = poem.equals(" Шаганэ ты моя, Шаганэ!Потому, что я с севера, что ли,Я готов рассказать тебе поле,Про волнистую рожь при луне.");
         System.out.println(result16);
     }
 }
