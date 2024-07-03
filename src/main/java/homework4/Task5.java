c82a171cc23cef918cb9959350378a93c1108a65
Kristy2594 <k.primakova2017@yandex.com>
2024-07-04 01:33:29 +0300

    homework3

--- src/main/java/homework4/Task5.java
+++ src/main/java/homework4/Task5.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Task5 {
 
-    // Данное упражнение так и не поняла. Как с помощью изученных тем сделать.
-    //Уже убежали на темы вперед...
-
 
     public static void main(String[] args) {
+        System.out.println("Введите пароль: ");
         String password = new Scanner(System.in).nextLine();
         if (password.length() >= 8) {
-            if (password.contains("!")) {
-                System.out.println("Пароль принят");
+            if (password.contains("!") || password.contains("@") || password.contains("#") || password.contains("$") || password.contains("%") || password.contains("^") || password.contains("&") || password.contains("*") || password.contains("No")) {
+                if (password.contains("1") || password.contains("2") || password.contains("3") || password.contains("4") || password.contains("5") || password.contains("6") || password.contains("7") || password.contains("8") || password.contains("9")) {
+                    System.out.println("Пароль принят");
+                } else {
+                    System.out.println("Пароль должен содержать минимум 1 цифру");
+                }
             } else {
                 System.out.println("Пароль должен содержать минимум 1 спецсимвол");
             }
@@ -21,6 +23,7 @@ public class Task5 {
         }
     }
 }
+
 
 
 
