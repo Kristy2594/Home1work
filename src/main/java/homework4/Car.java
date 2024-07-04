a9d48cf347d7e250831a5e1ac43fbb460b5c81e6
Kristy2594 <k.primakova2017@yandex.com>
2024-07-04 09:50:37 +0300

    homework4

---
+++ src/main/java/homework4/Car.java
@@ -0,0 +1,34 @@
+package homework4;
+
+public class Car {
+     String color;
+     int currentSpeed;
+     String brand;
+
+
+    public Car(String color, int currentSpeed,String brand) {
+        this.currentSpeed = 0;
+        this.color = color;
+        this.brand = brand;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+
+    }
+
+    public void speedUp(int speed) {
+        currentSpeed = currentSpeed + speed;
+
+    }
+
+    public void brake() {
+        if (currentSpeed <= 10) {
+            currentSpeed = 0;
+        } else {
+            currentSpeed = currentSpeed - 10;
+        }
+
+    }
+}
+
---
+++ src/main/java/homework4/CarProgram.java
@@ -0,0 +1,47 @@
+package homework4;
+
+public class CarProgram {
+    public static void main(String[] args) {
+
+        //2. Разгон автомобиля
+// Создайте класс Car
+// Поля могут быть любые.
+// Обязательно должно быть поле currentSpeed (тип данных int). По умолчанию currentSpeed = 0;
+// Создайте метод getCurrentSpeed. Метод должен возвращать (return) текущую скорость
+// Создайте метод speedUp, который принимает на вход число. Метод должен увеличивать текущую скорость на заданное число.
+// Создайте метод break(), который сокращает текущую скорость на 10 единиц.
+// Если текущая скорость <= 10, то метод делает текущую скорость равной 0
+// Создайте класс MyProgram + метод psvm.
+// В методе создайте новый экземпляр класса.
+// Запросите текущую скорость и выведите ее на экран.
+// Увеличьте скорость на 25 единиц.
+// Запросите текущую скорость и выведите ее на экран. Должно быть 25
+// Вызовите метод brake()
+// Запросите текущую скорость и выведите ее на экран. Должно быть 15
+// Вызовите метод brake()
+// Запросите текущую скорость и выведите ее на экран. Должно быть 5
+// Вызовите метод brake()
+// Запросите текущую скорость и выведите ее на экран. Должно быть 0
+
+        Car myNewCar = new Car( "белый", 12, "ауди" );
+
+        int speed=myNewCar.getCurrentSpeed();
+        System.out.println(speed);//тут0
+
+        myNewCar.speedUp(25);
+        speed=myNewCar.getCurrentSpeed();
+        System.out.println(speed);//тут25
+
+        myNewCar.brake();
+        speed=myNewCar.getCurrentSpeed();
+        System.out.println(speed);//тут15
+
+        myNewCar.brake();
+        speed=myNewCar.getCurrentSpeed();
+        System.out.println(speed);//тут5
+
+        myNewCar.brake();
+        speed=myNewCar.getCurrentSpeed();
+        System.out.println(speed);//тут0
+    }
+}
